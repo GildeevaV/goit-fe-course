968f4b8865c71d3ea6af41cc0b71db5775639097
Виктория <viktoria@MacBook-Air-Viktoria.local>
2020-01-16 11:49:42 +0200

    added homework-12

---
+++ goit-js/hw-12/src/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const _ = require('lodash');
+
+const _ = require('lodash/core');
+
+const fp = require('lodash/fp');
+ 
+const array = require('lodash/array');
+const object = require('lodash/fp/object');
+ 
+const at = require('lodash/at');
+const curryN = require('lodash/fp/curryN');
